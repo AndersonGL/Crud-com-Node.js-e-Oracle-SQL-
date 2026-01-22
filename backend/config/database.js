bd583443f44a57ff0eea56ea4c41211c101320ec
AndersonGL <98903039+AndersonGL@users.noreply.github.com>
2026-01-22 17:14:52 -0300

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,10 +4,10 @@ const oracledb = require('oracledb');
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
 const dbConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  connectString: process.env.DB_CONNECTION_STRING,
-   privilege: oracledb.SYSDBA 
+  user: process.env.DB_USER,  // usuarios_novo2
+  password: process.env.DB_PASSWORD,  // 465280
+  connectString: process.env.DB_CONNECTION_STRING, // 192.168.15.20:1521/XEXDB
+
 };
 
 module.exports.execute = async (sql, binds = {}, options = {}) => {
